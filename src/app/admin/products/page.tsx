8fa49160ab6ec3c5d399d9771e7bb772d9399ded
Anthony David <anthony.david34@sfr.fr>
2024-03-28 15:36:33 +0100

    admin product page

---
+++ src/app/admin/products/page.tsx
@@ -0,0 +1,39 @@
+import Link from "next/link";
+import { PageHeader } from "../_components/pageHeader";
+import { Button } from "@/components/ui/button";
+import { Table, TableBody, TableHead, TableHeader, TableRow } from "@/components/ui/table";
+
+export default function AdminProductsPage() {
+  return (
+    <>
+      <div className="flex justify-between items-center gap-4">
+        <PageHeader>Products</PageHeader>
+        <Button asChild>
+          <Link href="/admin/products/new">Add Product</Link>
+        </Button>
+      </div>
+      <ProductTable />
+    </>
+  );
+}
+
+function ProductTable() {
+  return (
+    <Table>
+      <TableHeader>
+        <TableRow>
+          <TableHead className="w-0">
+            <span className="sr-only">Available For Purchaise</span>
+          </TableHead>
+          <TableHead>Name</TableHead>
+          <TableHead>Price</TableHead>
+          <TableHead>Orders</TableHead>
+          <TableHead className="w-0">
+            <span className="sr-only">Actions</span>
+          </TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody></TableBody>
+    </Table>
+  );
+}
