cd9fa7afc645c255e81866b348a6d9f669616e77
Anthony David <anthony.david34@sfr.fr>
2024-04-02 09:44:38 +0200

    receipt Email

--- src/email/PurchaseReceipt.tsx
+++ src/email/PurchaseReceipt.tsx
@@ -1,3 +1,59 @@
-export default function PurchaseReceiptEmail() {
-    return 
+import {
+  Body,
+  Container,
+  Heading,
+  Head,
+  Html,
+  Preview,
+  Tailwind,
+} from "@react-email/components";
+import OrderInformation from "./components/OrderInformation";
+
+type PurchaseReceiptEmailProps = {
+  product: {
+    name: string;
+    description: string
+    imagePath: string;
+  };
+  order: { id: string; createdAt: Date; pricePaidInCents: number };
+  downloadVerificationId: string;
+};
+
+PurchaseReceiptEmail.PreviewProps = {
+  product: {
+    name: "Product name",
+    description: "Product Description",
+    imagePath: "/products/912c16ee-e660-4b13-8fa5-0fb994fc0d10-1.jpg",
+  },
+  order: {
+    id: crypto.randomUUID(),
+    createdAt: new Date(),
+    pricePaidInCents: 10000,
+  },
+  downloadVerificationId: crypto.randomUUID(),
+} satisfies PurchaseReceiptEmailProps;
+
+export default function PurchaseReceiptEmail({
+  product,
+  order,
+  downloadVerificationId,
+}: PurchaseReceiptEmailProps) {
+  return (
+    <Html>
+      <Preview>Download {product.name} and view receipt</Preview>
+      <Tailwind>
+        <Head />
+        <Body className="font-sans bg-white">
+          <Container className="max-w-xl">
+            <Heading>Purchase Receipt</Heading>
+            <OrderInformation
+              order={order}
+              product={product}
+              downloadVerificationId={downloadVerificationId}
+            />
+          </Container>
+        </Body>
+      </Tailwind>
+    </Html>
+  );
 }
